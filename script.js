8adcf9e684d6a1e20d71260613db4db580613a0e
Amit Kumar <84862054+Amit-kumar97@users.noreply.github.com>
2025-04-06 22:32:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const secondHighest = (arr) => {
+  if (!arr || arr.length <= 1) {
+    return -Infinity;
+  }
+
+  const uniqueElements = [...new Set(arr)];
+
+  if (uniqueElements.length <= 1) {
+    return -Infinity;
+  }
+
+  uniqueElements.sort((a, b) => b - a); // Sort in descending order
+
+  return uniqueElements[1];
+};
